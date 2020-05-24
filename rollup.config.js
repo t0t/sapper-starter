1d788f2b843a422bddba576c09b5254cb6b75933
Sergio Forés <sergiofores@gmail.com>
2020-05-24 23:13:57 +0200

    deploy to gh-pages

--- rollup.config.js
+++ rollup.config.js
@@ -7,20 +7,20 @@ import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import config from "sapper/config/rollup.js";
 import pkg from "./package.json";
-import sveltePreprocess from 'svelte-preprocess';
+import sveltePreprocess from "svelte-preprocess";
 
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const preprocess = sveltePreprocess({
-	scss: {
-	  includePaths: ['src'],
-	},
-	postcss: {
-	  plugins: [require('autoprefixer')],
-	},
-  });
+  scss: {
+    includePaths: ["src"],
+  },
+  postcss: {
+    plugins: [require("autoprefixer")],
+  },
+});
 
 const onwarn = (warning, onwarn) =>
   (warning.code === "CIRCULAR_DEPENDENCY" &&
